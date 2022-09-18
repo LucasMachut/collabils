6c653be27af24c842061aab55e2cce1b35d77d45
LucasMachut <machut.lucas@gmail.com>
2022-09-18 15:36:18 +0200

    fixed pb doctrine doesnt exist

--- src/Controller/Front/VideoController.php
+++ src/Controller/Front/VideoController.php
@@ -2,13 +2,19 @@
 
 namespace App\Controller\Front;
 
+use App\Entity\Video;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+
+
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Type;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
 class VideoController extends AbstractController
@@ -25,24 +31,44 @@ class VideoController extends AbstractController
     /**
      * @Route("/video/submit", name="video_submit")
      */
-    public function submit(): Response
+    public function submit (Request $request, ManagerRegistry $doctrine): Response
     {
-        $form = $this->createFormBuilder()
-        ->add('Titre', TextType::class, array(
+        $submitVideo = new Video();
+
+        $form = $this->createFormBuilder($submitVideo)
+        ->add('Title', TextType::class, array(
             'constraints' => new NotBlank(),
         ))
-        ->add('Définition', TextType::class, array(
+        ->add('Definition', TextType::class, array(
             'constraints' => new NotBlank(),
         ))
-        ->add('Contexte d\'utilisation', TextType::class, array(
+        ->add('Context', TextType::class, array(
             'constraints' => new NotBlank(),
         ))
-        ->add('Catégorie', DateType::class, array(
-            'constraints' => array(
-                new NotBlank(),
-            )
-        ))
+        ->add('Category', ChoiceType::class, [
+                'choices'  => [
+                    'Physique' => 'physics',
+                    'Psychologie' => 'psychology',
+                    'Informatique' => 'computer_science',
+                ],
+            ]   
+        )
+
         ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // On va faire appel au Manager de Doctrine
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($submitVideo);
+            $entityManager->flush();
+
+            // On redirige vers la liste
+            return $this->redirectToRoute('video_home');
+        }
+
         
         return $this->render('Front/video/submit.html.twig', [
             'controller_name' => 'VideoController',
