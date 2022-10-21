d751832b94dad5de458eb724565b6766230fb2ae
LucasMachut <machut.lucas@gmail.com>
2022-10-21 14:21:51 +0200

    page category list

--- src/Controller/Front/CategoryController.php
+++ src/Controller/Front/CategoryController.php
@@ -1,9 +1,14 @@
 <?php
 
 namespace App\Controller\Front;
-
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use App\Services\SlugService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
@@ -11,10 +16,14 @@ class CategoryController extends AbstractController
     /**
      * @Route("/category", name="category_list")
      */
-    public function list(): Response
+   
+    public function list(CategoryRepository $categoryRepository): Response
     {
+        $categories = $categoryRepository->findAll();
+
         return $this->render('Front/category/list.html.twig', [
-            'controller_name' => 'CategoryController',
+            'categories' => $categories,
         ]);
     }
+
 }
